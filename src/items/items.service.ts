cb53625e75989f95fc07bd25db2cb5240e2a5cab
Hair Zabala <hair.zabala@gmail.com>
2022-10-23 20:57:24 +1000

    basic CRUD with GraphQL

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
-import { CreateItemInput } from './dto/create-item.input';
-import { UpdateItemInput } from './dto/update-item.input';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateItemInput, UpdateItemInput } from './dto/';
+import { Item } from './entities/item.entity';
 
 @Injectable()
 export class ItemsService {
-  create(createItemInput: CreateItemInput) {
-    return 'This action adds a new item';
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+  ) {}
+
+  async create(createItemInput: CreateItemInput): Promise<Item> {
+    const item = this.itemsRepository.create(createItemInput);
+    return await this.itemsRepository.save(item);
   }
 
-  findAll() {
-    return `This action returns all items`;
+  findAll(): Promise<Item[]> {
+    // TODO: filtrar, paginar, por usuario, etc
+    return this.itemsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: string): Promise<Item> {
+    const item = await this.itemsRepository.findOneBy({ id });
+    if (!item) throw new BadRequestException(`Item with id ${id} not found`);
+    return item;
   }
 
-  update(id: number, updateItemInput: UpdateItemInput) {
-    return `This action updates a #${id} item`;
+  async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
+    console.log(updateItemInput);
+    const item = await this.itemsRepository.preload(updateItemInput);
+    console.log({ item });
+    return this.itemsRepository.save(item);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} item`;
+  async remove(id: string): Promise<Item> {
+    // TODO: soft delete, integridad referencial, etc
+    const item = await this.findOne(id);
+    await this.itemsRepository.remove(item);
+    return { ...item, id };
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+
+    this.logger.error(error);
+    // console.log(error)
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs',
+    );
   }
 }
